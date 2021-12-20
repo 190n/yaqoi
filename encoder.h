58f9dbde48042ad470b5a63f33f5b2d4acba8674
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-20 14:54:01 -0800

    Write encoder API

---
+++ encoder.h
@@ -0,0 +1,27 @@
+#ifndef __ENCODER_H__
+#define __ENCODER_H__
+
+#include "qoi.h"
+#include "pixel.h"
+#include "stats.h"
+
+#include <stdbool.h>
+#include <stdio.h>
+
+typedef struct Encoder Encoder;
+
+Encoder *encoder_create(FILE *output, bool track_stats, qoi_desc_t desc);
+
+void encoder_write_header(Encoder *e);
+
+void encoder_encode_pixels(Encoder *e, pixel_t *pixels, uint64_t n);
+
+void encoder_finish(Encoder *e);
+
+void encoder_flush(Encoder *e);
+
+qoi_stats_t encoder_get_stats(Encoder *e);
+
+void encoder_delete(Encoder **e);
+
+#endif
