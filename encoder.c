a9c75db75c63acae254600c0ddc90b072f0643ee
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-20 14:54:24 -0800

    Specify Encoder type and implement encoder_create and encoder_write_header

---
+++ encoder.c
@@ -0,0 +1,49 @@
+#include "encoder.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include "pixel.h"
+
+struct Encoder {
+	FILE *output;
+	bool track_stats;
+	pixel_t seen_pixels[64];
+	qoi_stats_t stats;
+	qoi_desc_t desc;
+};
+
+//
+// Create a new QOI encoder.
+//
+// output:      output file
+// track_stats: whether to track statistics during encoding
+// desc:        information about the file to encode
+//
+Encoder *encoder_create(FILE *output, bool track_stats, qoi_desc_t desc) {
+	Encoder *e = (Encoder *) calloc(1, sizeof(Encoder));
+	if (e) {
+		e->output = output;
+		e->track_stats = track_stats;
+		e->desc = desc;
+	}
+
+	return e;
+}
+
+//
+// Write the header of a QOI file.
+//
+// e: QOI encoder to use
+//
+void encoder_write_header(Encoder *e) {
+	if (e) {
+		qoi_header_t h;
+		memcpy(h.magic, "qoif", 4);
+		h.width = e->desc.width;
+		h.height = e->desc.height;
+		h.channels = e->desc.channels;
+		h.colorspace = e->desc.colorspace;
+		fwrite(&h, sizeof(qoi_header_t), 1, e->output);
+	}
+}
