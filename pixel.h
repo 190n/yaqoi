16f2c8b7fd147ba7ca20668bd60d563a9c294d9c
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-22 02:11:56 -0800

    Move pixel function into header

--- pixel.h
+++ pixel.h
@@ -7,6 +7,13 @@ typedef struct {
 	uint8_t r, g, b, a;
 } pixel_t;
 
-static inline uint8_t pixel_hash(pixel_t p);
+//
+// Compute the hash of a pixel, to determine its index in the array of seen pixels.
+//
+// p: the pixel to hash
+//
+static inline uint8_t pixel_hash(pixel_t p) {
+	return (p.r * 3 + p.g * 5 + p.b * 7 + p.a * 11) % 64;
+}
 
 #endif
