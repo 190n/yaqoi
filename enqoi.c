50799cd26917f902d4c038607ba8a8dba33320ea
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-22 02:22:30 -0800

    Test program to write a header

---
+++ enqoi.c
@@ -0,0 +1,12 @@
+#include "encoder.h"
+
+int main(void) {
+	Encoder *e = encoder_create("/dev/stdout", false, &(qoi_desc_t) {
+		.width = 1024,
+		.height = 2048,
+		.channels = RGB,
+		.colorspace = QOI_SRGB,
+	});
+	encoder_write_header(e);
+	encoder_delete(&e);
+}
