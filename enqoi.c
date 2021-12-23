41f19369d23fc69c64f469506c72e201008aebab
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-23 12:10:08 -0800

    Write a complete test image in enqoi

--- enqoi.c
+++ enqoi.c
@@ -2,12 +2,14 @@
 
 int main(void) {
 	Encoder *e = encoder_create(false, &(qoi_desc_t) {
-		.width = 0x01234567,
-		.height = 0xabcdef01,
+		.width = 2,
+		.height = 2,
 		.channels = RGB,
-		.colorspace = QOI_LINEAR,
+		.colorspace = QOI_SRGB,
 	});
 	encoder_write_header(stdout, e);
 	encoder_delete(&e);
+	fwrite("\xfe\xff\x00\x00\xfe\x00\xff\x00\xfe\x00\xff\x00", 1, 12, stdout);
+	fwrite("\x00\x00\x00\x00\x00\x00\x00\x01", 1, 8, stdout);
 	return 0;
 }
