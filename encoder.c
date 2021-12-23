439de49c80af2859e331ed3bcca8e9ad9d865938
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-22 16:46:55 -0800

    Disallow writing the header multiple times

--- encoder.c
+++ encoder.c
@@ -8,9 +8,13 @@
 
 struct Encoder {
 	bool track_stats;
-	pixel_t seen_pixels[64];
 	qoi_stats_t stats;
 	qoi_desc_t desc;
+	pixel_t seen_pixels[64];
+	pixel_t last_pixel;
+	uint8_t run_length;
+	bool header_written;
+	uint64_t pixels_written;
 };
 
 //
@@ -37,13 +41,17 @@ Encoder *encoder_create(bool track_stats, qoi_desc_t *desc) {
 //
 void encoder_write_header(FILE *dest, Encoder *e) {
 	if (e) {
+		if (e->header_written) {
+			return;
+		}
+		e->header_written = true;
 		qoi_header_t h;
 		store_u32be(h.magic, QOI_MAGIC);
 		store_u32be((char *) &h.width, e->desc.width);
 		store_u32be((char *) &h.height, e->desc.height);
 		h.channels = e->desc.channels;
 		h.colorspace = e->desc.colorspace;
-		fwrite(&h, sizeof(qoi_header_t), 1, dest);
+		fwrite(&h, QOI_HEADER_LENGTH, 1, dest);
 	}
 }
 
