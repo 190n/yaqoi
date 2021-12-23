2102b3efde7c50cfc00bcd8d316fcba5aa58c090
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-23 13:27:28 -0800

    Encoder progress

--- encoder.c
+++ encoder.c
@@ -28,6 +28,7 @@ Encoder *encoder_create(qoi_desc_t *desc) {
 	Encoder *e = (Encoder *) calloc(1, sizeof(Encoder));
 	if (e) {
 		e->desc = *desc;
+		e->stats.total_pixels = ((uint64_t) desc->width) * ((uint64_t) desc->height);
 	}
 
 	return e;
@@ -52,6 +53,66 @@ void encoder_write_header(FILE *dest, Encoder *e) {
 		h.channels = e->desc.channels;
 		h.colorspace = e->desc.colorspace;
 		fwrite(&h, QOI_HEADER_LENGTH, 1, dest);
+	}
+}
+
+void write_run(FILE *dest, Encoder *e) {
+	uint8_t chunk = QOI_OP_RUN;
+	chunk |= (e->run_length - 1);
+	fputc(chunk, dest);
+	e->stats.total_bits += 8;
+	e->stats.op_to_pixels.run += e->run_length;
+	e->run_length = 0;
+}
+
+void write_index(FILE *dest, Encoder *e, uint8_t index) {
+	uint8_t chunk = QOI_OP_INDEX;
+	chunk |= index;
+	fputc(chunk, dest);
+	e->stats.total_bits += 8;
+	e->stats.op_to_pixels.index++;
+}
+
+//
+// Encode some pixels using QOI.
+//
+// dest:   file to write encoded data to
+// e:      QOI encoder to use
+// pixels: array of pixels to encode
+// n:      number of pixels to encode
+//
+void encoder_encode_pixels(FILE *dest, Encoder *e, pixel_t *pixels, uint64_t n) {
+	if (e->end_marker_written) {
+		return;
+	}
+
+	for (uint64_t i = 0; i < n; i++) {
+		pixel_t p = pixels[i];
+		if (pixel_equal(p, e->last_pixel)) {
+			e->run_length++;
+			if (e->run_length == 62) {
+				// longest that a run can be
+				write_run(dest, e);
+			}
+
+			// pixel is now handled
+			continue;
+		} else if (e->run_length > 0) {
+			// pixel isn't equal, but there was a run going, so we have to end it
+			write_run(dest, e);
+			// we still have to figure out how to encode this pixel
+		}
+
+		uint8_t hash = pixel_hash(p);
+		if (pixel_equal(p, e->seen_pixels[hash])) {
+			// pixel is in our table, so use the index
+			write_index(dest, e, hash);
+			// handled
+			continue;
+		} else {
+			// store pixel in table
+			e->seen_pixels[hash] = p;
+		}
 	}
 }
 
