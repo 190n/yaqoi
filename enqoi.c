e5ec92171014656077cac8de813400c5783aeac3
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-25 17:13:41 -0800

    User-friendly STBI messages

--- enqoi.c
+++ enqoi.c
@@ -3,7 +3,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_ONLY_PNG
 #define STBI_ONLY_PNM
-// #define STBI_FAILURE_USERMSG
+#define STBI_FAILURE_USERMSG
 #include "stb_image.h"
 
 #include <getopt.h>
