96e0990377fcc0eb2ba0a3ed59a793c3916e7b39
Ben Grant <benjamin.j.grant@gmail.com>
2021-12-27 12:55:51 -0800

    Test pixel_subtract on case where I fail to use QOI_OP_LUMA

--- deqoi.c
+++ deqoi.c
@@ -5,12 +5,13 @@
 #include <stdio.h>
 
 int main(void) {
-	pixel_t a, b;
-	store_u32be((uint8_t *) &a.rgba, 0xff0000ff);
-	store_u32be((uint8_t *) &b.rgba, 0xfe0100ff);
+	pixel_t a;
+	a.channels.r = a.channels.g = a.channels.b = a.channels.a = 255;
+	pixel_t b = a;
+	b.channels.r = b.channels.g = b.channels.b = 0x0f;
 	pixel_difference_t diff = pixel_subtract(b, a);
 	printf("%" PRIu8 " %" PRIu8 " %" PRIu8 " %" PRIu8 "\n", b.channels.r, b.channels.g,
 	    b.channels.b, b.channels.a);
-	printf("%" PRId16 " %" PRId16 "\n", diff.r, diff.g);
+	printf("%" PRId16 " %" PRId16 " %" PRId16 " %" PRId16 "\n", diff.r, diff.g, diff.b, diff.a);
 	return 0;
 }
